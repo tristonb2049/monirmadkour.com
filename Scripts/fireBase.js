2c5766fbbda449ca446d5212ea1f4f8ec2600a48
sazar97 <tblaney97@comcast.net>
2019-09-12 23:15:53 -0500

    Firebase log-in now shows up

---
+++ Scripts/fireBase.js
@@ -0,0 +1,20 @@
+var app_firebase = {};
+(function(){
+
+    var firebaseConfig = {
+        apiKey: "AIzaSyCMpsNAT0eT5xJ3TjaqNa2NhFdYIYo5VtQ",
+        authDomain: "monir-madkour.firebaseapp.com",
+        databaseURL: "https://monir-madkour.firebaseio.com",
+        projectId: "monir-madkour",
+        storageBucket: "",
+        messagingSenderId: "358993289125",
+        appId: "1:358993289125:web:05b944eeb582c62c59924c"
+    };
+// Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    app_firebase = firebase;
+})()
+
+
+
