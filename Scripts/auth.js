9c38a1d6c14563232f9d17d20347d2136b5b68a5
sazar97 <tblaney97@comcast.net>
2019-09-20 00:01:31 -0500

    Need to get the redirect to work.

--- Scripts/auth.js
+++ Scripts/auth.js
@@ -1,22 +1,40 @@
 //sign-in
 
 const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    const email = loginForm['login-email'].value;
-    const password = loginForm['login-password'].value;
-    
+if(loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    firebase.auth().signInWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
-        
+        const email = loginForm['login-email'].value;
+        const password = loginForm['login-password'].value;
+
+
+        firebase.auth().signInWithEmailAndPassword(email, password).then(cred => {
+            console.log(cred.user);
+
+        });
     });
-});
+}
+
+/*
+function redirect(){
+    window.location = "../index.html";
+}
 
 //redirect after authentication success
     firebase.auth().onAuthStateChanged( user => {
         if(user){
-            window.location = "../index.html";
+            setTimeout(redirect, 4000);
         }
     });
+  */  
+//logout
+const logout = document.querySelector('#logout');
+if(logout) {
+    logout.addEventListener('click', (e) => {
+        e.preventDefault();
+        firebase.auth().signOut().then(() => {
+            console.log("Logged out successfully");
+        });
+    });
+}
