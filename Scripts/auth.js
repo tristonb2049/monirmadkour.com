08576aaa1791182ea3076e2defd208e5e4ae4bb1
sazar97 <tblaney97@comcast.net>
2019-09-27 22:14:18 -0500

    Added further links on dropdown menu and

--- Scripts/auth.js
+++ Scripts/auth.js
@@ -1,8 +1,19 @@
+//Get data
+firebase.firestore().collection('captions').get().then(snapshot => {
+    console.log(snapshot.docs);
+});
+
+
 
 //Listen for authorization changes
 firebase.auth().onAuthStateChanged(user => {
-    console.log(user);
-})
+    if(user){
+        console.log('User logged in: ', user);
+    }
+    else{
+        console.log('User Logged out');
+    }
+});
 
 
 //sign-in
@@ -17,31 +28,17 @@ if(loginForm) {
 
 
         firebase.auth().signInWithEmailAndPassword(email, password).then(cred => {
-            console.log(cred.user);
 
         });
     });
 }
 
-/*
-function redirect(){
-    window.location = "../index.html";
-}
 
-//redirect after authentication success
-    firebase.auth().onAuthStateChanged( user => {
-        if(user){
-            setTimeout(redirect, 4000);
-        }
-    });
-  */   
 //logout
 const logout = document.querySelector('#logout');
 if(logout) {
     logout.addEventListener('click', (e) => {
         e.preventDefault();
-        firebase.auth().signOut().then(() => {
-            console.log("Logged out successfully");
-        });
+        firebase.auth().signOut();
     });
 }
